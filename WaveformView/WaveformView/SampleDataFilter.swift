e12a3305533439a549ac55b47feab76718797a2a
changjianfeishui <329735967@qq.com>
2016-08-10 17:16:24 +0800

    sample

--- WaveformView/WaveformView/SampleDataFilter.swift
+++ WaveformView/WaveformView/SampleDataFilter.swift
@@ -16,29 +16,38 @@ class SampleDataFilter {
         self.data = sampleData
     }
     
+    //按照指定的尺寸约束来筛选数据
     func filteredSamplesForSize(size:CGSize) -> [Float] {
+        /* 最终需要展示的样本集 */
         var filteredSamples = [Float]()
-        //每个样本为16字节,得到样本数量
+        //1. 每个样本为16字节,得到样本数量
         let samplesCount = self.data.length/sizeof(Int16.self)
-        //某个宽度范围内显示多少个样本数量
+        //2. 某个宽度范围内显示多少个样本数量
         let binSize = Int(samplesCount / Int(size.width))
+        //3. 得到所有字节数据
+        /* 注意创建数组作为buffer时,要先分配好内存,即需要指定数组长度 */
         var bytes = [Int16](count:self.data.length,repeatedValue:0)
-        self.data.getBytes(&bytes, length: self.data.length * sizeof(Int16.self))
+        self.data.getBytes(&bytes, length: self.data.length)
+        //4. 以binSize为步长遍历所有样本,
         var maxSample: Int16 = 0
-        //遍历所有样本,
         for i in 0.stride(to: samplesCount-1, by: binSize) {
+            
             var sampleBin = [Int16](count:binSize,repeatedValue:0)
             for j in 0..<binSize {
+                /*小端存储,低字节序*/
                 sampleBin[j] = bytes[i + j].littleEndian
             }
+            //5. 获取每个尺寸单位样本集binSize中的最大样本
             let value = self.maxValue(in: sampleBin, ofSize: binSize)
+            //6. 添加到需要最终需要绘制展示的样本中
             filteredSamples.append(Float(value))
             if value > maxSample {
                 maxSample = value
             }
         }
-        
+        //7 .根据所有样本中的最大样本值进行缩放
         let scaleFactor = (size.height / 2.0) / CGFloat(maxSample)
+        //8. 对需要展示的样本进行缩放
         for i in 0..<filteredSamples.count {
             filteredSamples[i] = filteredSamples[i] * Float(scaleFactor)
         }
